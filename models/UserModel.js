8867b5c9b78ef1ef501d23105438a80f2a197736
Axel Fiol <axelfiol12@gmail.com>
2024-08-23 16:36:15 -0300

    commit 5 xD

--- models/UserModel.js
+++ models/UserModel.js
@@ -3,15 +3,19 @@ import sequelize from '../config/database.js';
 
 export default (sequelize) => {
   const User = sequelize.define('User', {
-  name: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  email: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true,
-  },
-}, {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+  }, {
   timestamps: true,
 })}
